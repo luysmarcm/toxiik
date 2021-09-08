7c35facc559245470ce96b33d1d8236e2d957cb6
luysmarcm <luysmar27@gmail.com>
2021-09-08 12:26:07 -0400

    Add NavBar.js

---
+++ components/NavBar.js
@@ -0,0 +1,80 @@
+import { Disclosure, Menu, Transition } from "@headlessui/react";
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
+
+const NavBar = ({ navigation }) => {
+	return (
+		<Disclosure as="nav" className="bg-black fixed z-50 w-full">
+			{({ open }) => (
+				<>
+					<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+						<div className="relative flex items-center justify-between h-16">
+							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+								{/* Mobile menu button*/}
+								<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+									<span className="sr-only">Open main menu</span>
+									{open ? (
+										<XIcon className="block h-6 w-6" aria-hidden="true" />
+									) : (
+										<MenuIcon className="block h-6 w-6" aria-hidden="true" />
+									)}
+								</Disclosure.Button>
+							</div>
+							<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+								<div className="flex-shrink-0 flex items-center">
+									<img
+										//className="w-10 h-1"
+										viewBox="0 0 24 24"
+										src="./images/tox.svg"
+										alt="logo"
+										width="30"
+									/>
+								</div>
+								<div className="hidden sm:block sm:ml-6">
+									<div className="flex space-x-4">
+										{navigation.map((item) => (
+											<a
+												key={item.name}
+												href={item.href}
+												// className={classNames(
+												// 	item.current
+												// 		? "bg-primary text-white"
+												// 		: "text-gray-300 hover:bg-primary hover:text-white",
+												// 	"px-3 py-2 rounded-md text-sm font-medium"
+												// )}
+												aria-current={item.current ? "page" : undefined}
+											>
+												{item.name}
+											</a>
+										))}
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+					<Disclosure.Panel className="sm:hidden">
+						<div className="px-2 pt-2 pb-3 space-y-1">
+							{navigation.map((item) => (
+								<a
+									key={item.name}
+									href={item.href}
+									// className={classNames(
+									// 	item.current
+									// 		? "bg-primary text-white"
+									// 		: "text-gray-300 hover:bg-primary hover:text-white",
+									// 	"block px-3 py-2 rounded-md text-base font-medium"
+									// )}
+									aria-current={item.current ? "page" : undefined}
+								>
+									{item.name}
+								</a>
+							))}
+						</div>
+					</Disclosure.Panel>
+				</>
+			)}
+		</Disclosure>
+	);
+};
+
+export default NavBar;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import AboutUs from "../components/AboutUs";
 import Footer from "../components/Footer";
 import { Header } from "../components/Header";
+import NavBar from "../components/NavBar";
 import Products from "../components/Products";
 import ToxiikUni from "../components/ToxiikUni";
 
@@ -9,17 +10,18 @@ import ToxiikUni from "../components/ToxiikUni";
 
 
 export default function Home() {
-	// const navigation = [
-	// 	{ name: "HOME", href: "#home" , current: true },
-	// 	{ name: "ABOUT US", href: "#about", current: false },
-	// 	{ name: "PRODUCTS", href: "#products", current: false },
-	// ];
+	const navigation = [
+		{ name: "HOME", href: "#home" , current: true },
+		{ name: "ABOUT US", href: "#about", current: false },
+		{ name: "PRODUCTS", href: "#products", current: false },
+	];
 	return (
 		<div>
 			<Head>
 				<title>Toxiik</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
+			<NavBar/>
 			<Header />
 			<AboutUs />
 			<Products/>
