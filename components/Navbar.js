e38a37277c7b2b48df4d6bcf5c9b9e0aa7cca001
luysmarcm <luysmar27@gmail.com>
2021-09-08 11:05:24 -0400

    NavBar Prueba

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,83 +1,56 @@
-import { Disclosure, Menu, Transition } from "@headlessui/react";
-import {MenuIcon, XIcon } from "@heroicons/react/outline";
+import React from "react";
 
-
-
-
-
-const NavBar = ({navigation}) => {
+const NavBar = () => {
 	return (
-		<Disclosure as="nav" className="bg-black fixed z-50 w-full">
-			{({ open }) => (
-				<>
-					<div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-						<div className="relative flex items-center justify-between h-16">
-							<div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-								{/* Mobile menu button*/}
-								<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-									<span className="sr-only">Open main menu</span>
-									{open ? (
-										<XIcon className="block h-6 w-6" aria-hidden="true" />
-									) : (
-										<MenuIcon className="block h-6 w-6" aria-hidden="true" />
-									)}
-								</Disclosure.Button>
-							</div>
-							<div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-								<div className="flex-shrink-0 flex items-center">
-									<img
-										//className="w-10 h-1"
-										viewBox="0 0 24 24"
-										src="./images/tox.svg"
-										alt="logo"
-										width="30"
-									/>
-								</div>
-								<div className="hidden sm:block sm:ml-6">
-									<div className="flex space-x-4">
-										{navigation.map((item) => (
-											<a
-												key={item.name}
-												href={item.href}
-												// className={classNames(
-												// 	item.current
-												// 		? "bg-primary text-white"
-												// 		: "text-gray-300 hover:bg-primary hover:text-white",
-												// 	"px-3 py-2 rounded-md text-sm font-medium"
-												// )}
-												aria-current={item.current ? "page" : undefined}
-											>
-												{item.name}
-											</a>
-										))}
-									</div>
-								</div>
-							</div>
-						</div>
-					</div>
+		<div className="w-full">
+			<div className="fixed z-50 w-full bg-black top-0 text-white body-font flex flex-wrap items-center justify-between px-2 py-3 shadow-lg">
+				{/* <header class="text-white body-font bg-black bg-fixed"> */}
+				<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+					<a>
+						<img
+							//className="w-10 h-1"
+							viewBox="0 0 24 24"
+							src="./images/tox.svg"
+							alt="logo"
+							width="30"
+						/>
+					</a>
+					<nav class="md:ml-auto flex flex-wrap items-center text-base font-mono font-extrabold cursor-pointer justify-center">
+						<a href="#home" class="mr-5 hover:text-primary">
+							HOME
+						</a>
+						<a href="#about" class="mr-5 hover:text-primary">
+							ABOUT US
+						</a>
+						<a href="#products" class="mr-5 hover:text-primary">
+							PRODUCTS
+						</a>
+					</nav>
+				</div>
+				{/* </header> */}
+			</div>
+		</div>
 
-					<Disclosure.Panel className="sm:hidden">
-						<div className="px-2 pt-2 pb-3 space-y-1">
-							{navigation.map((item) => (
-								<a
-									key={item.name}
-									href={item.href}
-									// className={classNames(
-									// 	item.current
-									// 		? "bg-primary text-white"
-									// 		: "text-gray-300 hover:bg-primary hover:text-white",
-									// 	"block px-3 py-2 rounded-md text-base font-medium"
-									// )}
-									aria-current={item.current ? "page" : undefined}
-								>
-									{item.name}
-								</a>
-							))}
-						</div>
-					</Disclosure.Panel>
-				</>
-			)}
-		</Disclosure>
+		// <div>
+		// 	<header class="text-white body-font bg-black bg-fixed">
+		// 		<div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+		// 			<a>
+		// 				<img
+		// 					//className="w-10 h-1"
+		// 					viewBox="0 0 24 24"
+		// 					src="./images/tox.svg"
+		// 					alt="logo"
+		// 					width="30"
+		// 				/>
+		// 			</a>
+		// 			<nav class="md:ml-auto flex flex-wrap items-center text-base font-mono font-extrabold cursor-pointer justify-center">
+		// 				<a class="mr-5 hover:text-primary">HOME</a>
+		// 				<a class="mr-5 hover:text-primary">ABOUT US</a>
+		// 				<a class="mr-5 hover:text-primary">PRODUCTS</a>
+		// 			</nav>
+		// 		</div>
+		// 	</header>
+		// </div>
 	);
 };
 
