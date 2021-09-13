1bf3bbac69bca513083d4f1e3d0448db3440919e
luysmarcm <luysmar27@gmail.com>
2021-09-13 16:13:44 -0400

    products

--- components/Header.js
+++ components/Header.js
@@ -11,12 +11,12 @@ export const Header = () => {
 					// }}
 				>
 					<div className=" z-20 bg-toxiik bg-no-repeat bg-auto bg-center  lg:-translate-y-16 lg:bg-top lg:pb-10 lg:pt-48">
-						<div className=" relative z-30 bg-monta bg-cover h-12 translate-y-32 bg-center lg:h-auto py-72 lg:translate-y-1 ">
+						<div className=" relative z-30 bg-monta bg-cover h-12 translate-y-36 bg-center lg:h-auto py-72 lg:translate-y-1 ">
 							<div
 								class="absolute z-40 inset-x-0 bottom-0  bg-letra bg-auto bg-no-repeat bg-center
-								h-5
+								h-3
 								-translate-y-48
-								lg:h-11 lg:-translate-y-24"
+								lg:h-7 lg:-translate-y-24"
 							></div>
 						</div>
 					</div>
--- components/Products.js
+++ components/Products.js
@@ -5,27 +5,31 @@ const Products = () => {
 		<section id="products" class="relative w-auto py-10">
 			<div className="text-primary font-mont font-extrabold py-16 text-center text-4xl lg:text-8x1">
 				OUR PRODUCTS
+				<div className="stroke-current text-current">
+					{" "}
+					Text
+				</div>
 			</div>
-			<div  className="w-scream grid grid-cols-4 bg-blue-400 ">
+			<div className="w-scream grid grid-cols-4 ">
 				<div className="flex flex-col col-span-4 md:col-span-2 lg:row-span-2 lg:p-14">
 					<div className="flex-1 aspect-w-4 aspect-h-3 lg:aspect-h-4">
 						{/* <img className="object-cover" src="./images/gris.png" alt="" /> */}
-						<div className="flex flex-col p-8 lg:flex-row items-center">
+						<div className="flex flex-col p-12 lg:flex-row items-center">
 							<img
-								className="object-cover sm:w-8/12 sm:h-3/5 md:w-8/12 md:h-3/5"
+								className="object-cover h-80 lg:h-full"
 								src="./images/cargadorp.jpeg"
 								alt=""
 								//width="200px"
 							/>
-							<div className="flex-col -space-y-2">
+							<div className="flex-col -space-y-2 p-10 lg:p-0 ">
 								<div className="text-black font-mont font-normal text-2xl lx:text-1xl">
 									FAST
 								</div>
 
-								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-6xl ">
+								<div className="font-mont font-extrabold bg-clip-text text-transparent text-5xl bg-fondoT bg-cover bg-left lg:text-8xl ">
 									CHAR
 								</div>
-								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-6xl ">
+								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent text-5xl	bg-fondoT bg-cover lg:text-8xl ">
 									GER
 								</div>
 								{/* <div className="text-black font-mont font-extrabold text-2xl lx:text-1xl">
@@ -38,22 +42,22 @@ const Products = () => {
 
 				<div className="flex flex-col col-span-4 md:col-span-2 xl:row-span-1 xl:col-span-1">
 					<div className="flex-1 aspect-w-4 aspect-h-3 ">
-						<div className="flex flex-col p-8 lg:flex-row items-center">
+						<div className="flex flex-col p-5 lg:flex-row items-center">
 							<img
-								className="object-cover"
+								className="object-cover h-80 lg:h-80"
 								src="./images/cargadorp.jpeg"
 								alt=""
-								width="100px"
+								//width="100px"
 							/>
-							<div className="flex-col -space-y-2">
+							<div className="flex-col -space-y-2  p-10 lg:p-0">
 								<div className="text-black font-mont font-normal text-2xl lx:text-1xl">
 									FAST
 								</div>
 
-								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-6xl ">
+								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-5xl ">
 									CHAR
 								</div>
-								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-6xl ">
+								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-5xl ">
 									GER
 								</div>
 								{/* <div className="text-black font-mont font-extrabold text-2xl lx:text-1xl">
@@ -66,22 +70,22 @@ const Products = () => {
 
 				<div className="flex flex-col col-span-4 md:col-span-2 xl:row-span-1 xl:col-span-1">
 					<div className="flex-1 aspect-w-4 aspect-h-3 ">
-						<div className="flex flex-col p-8 lg:flex-row items-center">
+						<div className="flex flex-col p-5 lg:flex-row items-center">
 							<img
-								className="object-cover"
+								className="object-cover h-80 lg:h-80"
 								src="./images/cargadorp.jpeg"
 								alt=""
-								width="100px"
+								//width="100px"
 							/>
-							<div className="flex-col -space-y-2">
+							<div className="flex-col -space-y-2  p-10 lg:p-0">
 								<div className="text-black font-mont font-normal text-2xl lx:text-1xl">
 									FAST
 								</div>
 
-								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-6xl ">
+								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-5xl ">
 									CHAR
 								</div>
-								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-6xl ">
+								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-5xl ">
 									GER
 								</div>
 								{/* <div className="text-black font-mont font-extrabold text-2xl lx:text-1xl">
@@ -93,22 +97,22 @@ const Products = () => {
 				</div>
 				<div className="flex flex-col col-span-4 md:col-span-2 xl:row-span-1 xl:col-span-1 ">
 					<div className="flex-1 aspect-w-4 aspect-h-3 ">
-						<div className="flex flex-col p-8 lg:flex-row items-center">
+						<div className="flex flex-col p-5 lg:flex-row items-center">
 							<img
-								className="object-cover"
+								className="object-cover h-80 lg:h-80"
 								src="./images/cargadorp.jpeg"
 								alt=""
-								width="100px"
+								//width="100px"
 							/>
-							<div className="flex-col -space-y-2">
+							<div className="flex-col -space-y-2 p-10 lg:p-0">
 								<div className="text-black font-mont font-normal text-2xl lx:text-1xl">
 									FAST
 								</div>
 
-								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-6xl ">
+								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-5xl ">
 									CHAR
 								</div>
-								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-6xl ">
+								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-5xl ">
 									GER
 								</div>
 								{/* <div className="text-black font-mont font-extrabold text-2xl lx:text-1xl">
@@ -120,22 +124,22 @@ const Products = () => {
 				</div>
 				<div className="flex flex-col col-span-4 md:col-span-2 xl:row-span-1 xl:col-span-1 ">
 					<div className="flex-1 aspect-w-2 aspect-h-1 ">
-						<div className="flex flex-col p-8 lg:flex-row items-center">
+						<div className="flex flex-col p-5 lg:flex-row items-center">
 							<img
-								className="object-cover"
+								className="object-cover h-80 lg:h-80"
 								src="./images/cargadorp.jpeg"
 								alt=""
-								width="100px"
+								//width="100px"
 							/>
-							<div className="flex-col -space-y-2">
+							<div className="flex-col -space-y-2  p-10 lg:p-0">
 								<div className="text-black font-mont font-normal text-2xl lx:text-1xl">
 									FAST
 								</div>
 
-								<div className="font-mont font-extrabold bg-clip-text text-transparent	bg-fondoT bg-cover bg-left text-6xl ">
+								<div className="font-mont font-extrabold bg-clip-text text-transparent bg-fondoT bg-cover bg-left text-5xl ">
 									CHAR
 								</div>
-								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-6xl ">
+								<div className="text-black font-mont font-extrabold bg-left-top bg-clip-text text-transparent	bg-fondoT bg-cover text-5xl ">
 									GER
 								</div>
 								{/* <div className="text-black font-mont font-extrabold text-2xl lx:text-1xl">
